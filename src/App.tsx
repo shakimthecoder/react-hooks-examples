efbcf3c9c365e4a0b3ad36aaa9602fe52f6d72b8
Shakim Harris <amenraprod@gmail.com>
2025-03-21 21:00:18 -0400

    imported component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import CountExample from './assets/hooks/1-use-state'
+import CountExample from './assets/hooks/useState/1-use-state'
+import EffectExample from './assets/hooks/2-use-effect'
 import './App.css'
 
 function App() {
-  return (
+  return (<>
   <CountExample />
+  <EffectExample />
+  </>
   );
   }
 
