2b68ab43ee9db938670a551c18baf4aea9afe028
Shakim Harris <amenraprod@gmail.com>
2025-03-22 17:22:47 -0400

    add fn

--- src/assets/hooks/4-use-reducer/use-reducer.tsx
+++ src/assets/hooks/4-use-reducer/use-reducer.tsx
@@ -1,12 +1,14 @@
 import { useState, useReducer } from 'react';
 
 export const ReducerExample = () => {
-    let [count, setCount ] = useState<number>(0);
+
+    const reducer = () => {}
+    let [state, dispatch ] = useReducer(reducer, { count: 0})
     return (
         <div>
         <p>Count: {count} </p>
-        <button onClick={() => setCount((prev) => prev + 1)}> Click to count</button>
-        <button onClick={() => setCount((prev) => prev +1)}>Click to count</button>
+        <button onClick={() => setCount((prev) => prev + 1)}> + </button>
+        <button onClick={() => setCount((prev) => prev - 1)}> - </button>
         </div>
     )
 }
