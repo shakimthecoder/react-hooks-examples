470f2ef6ec69a900a07a6fe44edc8c4ebb62c8e7
Shakim Harris <amenraprod@gmail.com>
2025-03-22 19:07:52 -0400

    import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import CountExample from './assets/hooks/useState/1-use-state'
 import EffectExample from './assets/hooks/useeffect/2-use-effect'
-import { ReducerExample } from './assets/hooks/4-use-reducer/use-reducer'
+import { ReducerExample, SecondRefExample } from './assets/hooks/4-use-reducer/use-reducer'
 import { RefExample } from './assets/hooks/5-use-ref/5-use-ref'
 import './App.css'
 
@@ -12,7 +12,7 @@ function App() {
   <CountExample />
   <EffectExample />
   <ReducerExample />
-  <RefExample />
+  <SecondRefExample />
 
   </>
   );
