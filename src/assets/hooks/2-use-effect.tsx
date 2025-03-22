c7ecaf4645b19fc69ef442c77baa06807dcf3df4
Shakim Harris <amenraprod@gmail.com>
2025-03-21 21:08:58 -0400

    fetched titles

--- src/assets/hooks/2-use-effect.tsx
+++ src/assets/hooks/2-use-effect.tsx
@@ -1,21 +1,20 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function EffectExample() {
+    let [data, setData] = useState<[]>([]);
    useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => response.json()
-    .then((json) => console.log(json)))
+    .then((json) => setData(json)))
    }, [])
 
     return( <div>
         {""}
         <h1>Posts</h1>
             <ul>
-                <li>Post 1</li>
-                <li>Post 2</li>
-                <li>Post 3</li>
-                <li>Post 4</li>
-                <li>Post 5</li>
+             {data.map((item: any) => (
+             <li>{item.title}</li>
+             ))}
             </ul>
             </div>
         )
