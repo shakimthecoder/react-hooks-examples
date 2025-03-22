4bd3dad1020ea72fe7fc4ab9b94eb346981154bb
Shakim Harris <amenraprod@gmail.com>
2025-03-22 17:16:09 -0400

    text

--- src/assets/hooks/4-use-reducer/use-reducer.tsx
+++ src/assets/hooks/4-use-reducer/use-reducer.tsx
@@ -5,8 +5,8 @@ export const ReducerExample = () => {
     return (
         <div>
         <p>Count: {count} </p>
-        <button onClick={() => setCount((prev) => prev + 1)} ></button>
-        <button onClick={() => setCount((prev) => prev +1)}></button>
+        <button onClick={() => setCount((prev) => prev + 1)}> Click to count</button>
+        <button onClick={() => setCount((prev) => prev +1)}>Click to count</button>
         </div>
     )
 }
