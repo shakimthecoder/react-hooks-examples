f8c97ae6a45c01f2bde59e54a591633a282f157f
Shakim Harris <amenraprod@gmail.com>
2025-03-21 20:30:55 -0400

    added test

---
+++ src/assets/hooks/useState/use-state.test.ts
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CountExample from './1-use-state';
+
+test('loads and displays count', async () => {
+    // ARRANGE
+
+    // ACT 
+
+
+    // ASSERT
+
+}
