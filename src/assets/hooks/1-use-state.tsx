f09135c3fadd7e93d9259faa318b32da72ef2b43
Shakim Harris <amenraprod@gmail.com>
2025-03-21 20:10:34 -0400

    finsished counter

--- src/assets/hooks/1-use-state.tsx
+++ src/assets/hooks/1-use-state.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react';
 
 export default function CountExample(): any {
     let count = 0;
+    let [counter, setCounter] = useState<number>(0);
 
     const increaseCount = () => {
-        count += 1;
+        counter += 1;
         console.log(count);
+
+        setCounter((prev) => prev + 1);
     }
 
     return (
         <div>
-        <p>Count: { count }</p>
+        <p>Count: { counter }</p>
         <button onClick={increaseCount}>Increase count</button>
     </div>)
    
