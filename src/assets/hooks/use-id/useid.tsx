38444486068c9f7f7b6cd8be904a767666396bc2
Shakim Harris <amenraprod@gmail.com>
2025-03-22 20:25:44 -0400

    added another id

--- src/assets/hooks/use-id/useid.tsx
+++ src/assets/hooks/use-id/useid.tsx
@@ -1,11 +1,15 @@
 import { useId } from 'react';
 
-export const useIdExample = () => {
+export const UseIdExample = () => {
     const id = useId();
+    const id2 = useId();
     return (
-        <div>
-        <label htmlFor={id}>Enter your name:</label>
-        <input id={id} type="text"></input>
-        </div>
+        <><div>
+            <label htmlFor={id}>Enter your name:</label>
+            <input id={id} type="text"></input>
+        </div><div>
+                <label htmlFor={id2}>Enter your password:</label>
+                <input id={id2} type="text"></input>
+            </div></>
     )
 }
