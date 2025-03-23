50ddb70b641696328e43d198e89cf25d899d7d8e
Shakim Harris <amenraprod@gmail.com>
2025-03-22 21:52:41 -0400

    wrapped filter in hook

--- src/assets/usetransition/8-use-transition.tsx
+++ src/assets/usetransition/8-use-transition.tsx
@@ -3,14 +3,18 @@ import { useTransition, useState } from 'react';
 export const useTransitionExample = () => {
     const [query, setQuery] = useState("");
     const [filteredItems, setFilteredItems] = useState([]);
+    const [isPending, startTransition] = useTransition();
     const items = Array.from({ length: 10000 }, (_,i) => `Item ${i + 1}`);
     const handleFilter = (e) => {
         const value = e.target.value;
         setQuery(value);
     }
+    startTransition(() => {
     const filtered: any = items.filter((item) =>
         item.toLowerCase().includes(item.valueOf()));
         setFilteredItems(filtered);
+    });
+    
     return (
         <div>
         <input 
