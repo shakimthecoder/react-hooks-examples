6d203df833e78ef3dec5dda3a055ef7bf43bbaa1
Shakim Harris <amenraprod@gmail.com>
2025-03-22 22:17:03 -0400

    cleanup

--- src/assets/hooks/useDeferred/usedeffered.tsx
+++ src/assets/hooks/useDeferred/usedeffered.tsx
@@ -1,35 +1,5 @@
-import { useTransition, useState } from 'react';
+import { useTransition, useDeferredValue, useState } from 'react';
+// 
+export const useDeferredExample = () => {
 
-export const UseTransitionExample = () => {
-    const [query, setQuery] = useState("");
-    const [filteredItems, setFilteredItems] = useState([]);
-    const [isPending, startTransition] = useTransition();
-    const items = Array.from({ length: 10000 }, (_,i) => `Item ${i + 1}`);
-    const handleFilter = (e) => {
-        const value = e.target.value;
-        setQuery(value);
-    }
-   // startTransition(() => {
-    const filtered: any = items.filter((item) => {
-        item.toLowerCase().includes(item.valueOf());
-        setFilteredItems(filtered);
-    });
-    
-    return (
-        <div>
-        <input 
-         type="text"
-         value={query}
-         onChange={handleFilter}
-         placeholder="Search items..."
-         >
-          <ul>
-            {filteredItems.map((item, index) => (<li key={index}>{item}
-            </li> 
-            ))}
-            {isPending && <p>Loading....</p>}
-            </ul>
-         </input>
-        </div>
-    )
-        }
+}
