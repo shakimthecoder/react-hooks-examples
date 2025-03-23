f00ef9b21437ae82645c54c887959e5eb4c57d7e
Shakim Harris <amenraprod@gmail.com>
2025-03-22 22:24:46 -0400

    showed example

--- src/assets/hooks/useDeferred/usedeffered.tsx
+++ src/assets/hooks/useDeferred/usedeffered.tsx
@@ -1,5 +1,21 @@
 import { useTransition, useDeferredValue, useState } from 'react';
 // 
 export const useDeferredExample = () => {
+const [query, setQuery] = useState()
+ const deferredQuery = useDeferredValue(query);
 
+// not doing data filtering but the point is shown in the ternary statement
+
+ return (
+    <div>
+        <input 
+         type="text"
+         value={query}
+         placeholder="Cooking..."
+         >
+        {deferredQuery !== query && <p>Loading....</p>}
+         </input>
+    
+    </div>
+ )
 }
