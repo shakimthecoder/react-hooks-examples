672dbab279f70d62074cea97d7382d331d6f8d73
Shakim Harris <amenraprod@gmail.com>
2025-03-22 21:17:13 -0400

    rm -rf

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ import EffectExample from './assets/hooks/useeffect/2-use-effect'
 import { ReducerExample, SecondRefExample } from './assets/hooks/4-use-reducer/use-reducer'
 import { RefExample } from './assets/hooks/5-use-ref/5-use-ref'
 import { LayoutEffectExample } from './assets/hooks/7-layout.tsx/layout'
+import { UseIdExample } from './assets/hooks/use-id/useid';
 import './App.css'
 
 function App() {
   return (<>
-  <CountExample />
-  <EffectExample />
-  <ReducerExample />
-  <SecondRefExample />
-  <LayoutEffectExample />
+  <UseIdExample />
+
 
 
   </>
